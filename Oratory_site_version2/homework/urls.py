bc36a9c9241cf5c66d8f99519dd49240571fe678
Taneesh <taneeshgnav@gmail.com>
2020-05-12 13:28:35 +0530

    secondversion

---
+++ Oratory_site_version2/homework/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'homework'
+
+urlpatterns = [
+    #path('index/',views.index,name='homeworkIndex'),
+    path('',views.assignhomework,name='assignhomework'),
+    path('<int:sender_id>/<int:reciever_id>/',views.sendhomework,name='sendhomework')
+]
